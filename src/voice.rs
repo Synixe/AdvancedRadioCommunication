fcb020c4b1aec0e319b633d8b8a65ef15f94f835
Brett <brett@bmandesigns.com>
2018-02-07 23:29:14 -0600

    Working distance

--- src/voice.rs
+++ src/voice.rs
@@ -7,12 +7,13 @@ use serenity::voice::AudioReceiver;
 extern crate typemap;
 use self::typemap::Key;
 
+use std::cmp;
 use std::collections::HashMap;
-use std::sync::Arc;
 use std::f32;
+use std::sync::Arc;
 
-use player::Vector3;
 use player::Player;
+use player::Vector3;
 
 pub struct VoiceManager;
 impl Key for VoiceManager {
@@ -24,12 +25,13 @@ pub struct Receiver<'a> {
     ids: HashMap<u32, String>,
     players: Arc<Mutex<HashMap<String, Player>>>,
     sources: HashMap<u32, openal::source::Stream<'a>>,
-    buffer: [i16; 1920]
+    buffer: [i16; 960]
 }
 impl <'a>Receiver<'a> {
     pub fn new(players: Arc<Mutex<HashMap<String, Player>>>) -> Self {
-        let listener = openal::listener::default(&Default::default()).unwrap();
-        let mut buf: [i16; 1920] = [0; 1920];
+        let mut listener = openal::listener::default(&Default::default()).unwrap();
+        listener.set_position(&openal::Position(openal::Vector{x: 0.0, y: 0.0, z: 0.0}));
+        let mut buf: [i16; 960] = [0; 960];
         Self {
             listener: listener,
             ids: HashMap::new(),
@@ -56,8 +58,7 @@ impl <'a>AudioReceiver for Receiver<'a> {
                                 let position = player.get_position();
                                 let orientation = player.get_orientation();
                                 drop(player);
-                                process_ild(data, &position, &orientation, source, &mut self.buffer, stereo);
-                                //source.push(2, data, 48000);
+                                process(data, &position, &orientation, source, &mut self.buffer, stereo);
                                 if source.state() != openal::source::State::Playing {
                                     source.play();
                                 }
@@ -75,27 +76,19 @@ impl <'a>AudioReceiver for Receiver<'a> {
             }
         }
         if create {
-            self.sources.insert(ssrc, self.listener.source().unwrap().stream());
+            let mut stream = self.listener.source().unwrap().stream();
+            stream.enable_relative();
+            self.sources.insert(ssrc, stream);
         }
     }
 }
 
-fn process_ild(data: &[i16], pos: &Vector3, orientation: &Vector3, source: &mut openal::source::Stream, buf: &mut [i16; 1920], stereo: bool) {
-    if stereo {
-        //let angle = orientation.x * f32::consts::PI / 180.0;
-        let mut dir = pos.y.atan2(pos.x);
-        while dir > f32::consts::PI {
-            dir = dir - (2.0 * f32::consts::PI);
-        }
-        let gain_left   = -0.375 * dir.cos() + 0.625;
-        let gain_right   = 0.375 * dir.cos() + 0.625;
-        for i in 0..(data.len()) {
-            if i % 2 == 0 {
-                buf[i] = (data[i] as f32 * gain_left) as i16;
-            } else {
-                buf[i] = (data[i] as f32 * gain_right) as i16;
-            }
-        }
-        source.push(2, buf, 48000);
+fn process(data: &[i16], pos: &Vector3, orientation: &Vector3, source: &mut openal::source::Stream, buf: &mut [i16; 960], stereo: bool) {
+    let mut i = 0;
+    while i < data.len() / 2 {
+        buf[i] = (data[i * 2] / 2) + (data[(i * 2) + 1 ] / 2);
+        i += 1;
     }
+    source.set_position(&openal::Position(openal::Vector{x: pos.x, y: pos.y, z: pos.z}));
+    source.push(1, buf, 48000);
 }
